2601afece7d9cbae925539e36ac7e2195195e4d9
Junaid Hossain <junaid.hossain@infinitibit.com>
2025-11-13 17:36:51 +0600

    feat: added chat completion, embeddings, similarity score

--- examples/test.py
+++ examples/test.py
@@ -1,25 +1,60 @@
+import os
 from graphbit import Huggingface
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Initialize Hugging Face integration
-hf = Huggingface(api_token="")
+hf = Huggingface(os.getenv("HUGGINGFACE_API_KEY"))
 
-# # Generate text using a model
+messages=[
+        {"role": "user", "content": "What is langchain?"},
+    ]
+
+# Generate text using a model
 # response = hf.inference.generate_text(
 #     model="openai/gpt-oss-20b",
-#     prompt="Once upon a time in a world where AI ruled the cities,",
-#     max_new_tokens=50,
+#     prompt="What is langchain?",
+#     max_tokens=50,
 #     temperature=0.8
 # )
 
 # print("Generated Text:\n", response)
+# print("\n\n\n")
+# print("Content: ", response.choices[0].message.content)
 
-# chat with a model
-response = hf.llm.chat(
-    model="google/gemma-2-2b-it",
-    messages=[
-        {"role": "user", "content": "Hello, how are you?"},
-        {"role": "assistant", "content": "I'm doing well, thank you!"},
-        {"role": "user", "content": "Can you help me with Python programming?"}
-    ]
+# CHAT WITH A MODEL
+# response = hf.llm.chat(
+#     model="moonshotai/Kimi-K2-Thinking",
+#     messages=[
+#         {"role": "user", "content": "What is langchain?"},
+#     ],
+#     max_tokens=500,
+#     temperature=0.5
+# )
+# print("\nChat Response:\n", response)
+# print("\n\n\n")
+
+# result = hf.llm.get_output_content(response)
+
+# print("Content:\n", result)
+
+# EMBEDDINGS EXAMPLE
+# text = "This is an example of embedding generation."
+# embeddings = hf.embeddings.embed(model="sentence-transformers/all-MiniLM-L6-v2", text=text)
+# print(embeddings)
+
+# SIMILARITY EXAMPLE
+sentence = "Machine learning is so easy."
+other_sentences=[
+    "Deep learning is so straightforward.",
+    "This is so difficult, like rocket science.",
+    "I can't believe how much I struggled with this.",
+]
+similarities = hf.embeddings.similarity(
+    model="sentence-transformers/all-MiniLM-L6-v2", 
+    sentence=sentence, 
+    other_sentences=other_sentences
 )
-print("\nChat Response:\n", response)
+
+print(similarities)
--- python/python-src/graphbit/integrations/huggingface/embeddings.py
+++ python/python-src/graphbit/integrations/huggingface/embeddings.py
@@ -1,15 +1,25 @@
-import requests
+from huggingface_hub import InferenceClient
 
 class HuggingfaceEmbeddings:
-    def __init__(self, token: str):
-        self.token = token
+    def __init__(self, token: str, **kwargs):
+        self.client = InferenceClient(token=token, **kwargs)
 
-    def embed(self, model: str, text: str):
-        headers = {"Authorization": f"Bearer {self.token}"}
-        response = requests.post(
-            f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model}",
-            headers=headers,
-            json={"inputs": text}
-        )
-        response.raise_for_status()
-        return response.json()
+    # def embed(self, model: str, text: str):
+    #     headers = {"Authorization": f"Bearer {self.token}"}
+    #     response = requests.post(
+    #         f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model}",
+    #         headers=headers,
+    #         json={"inputs": text}
+    #     )
+    #     response.raise_for_status()
+    #     return response.json()
+
+    def embed(self, model: str, text: str, **kwargs):
+        '''Embed the given text using a pre-trained model on Hugging Face Hub.'''''
+        embeddings = self.client.feature_extraction(model=model, text=text, **kwargs)
+        return embeddings
+    
+    def similarity(self, model: str, sentence: str, other_sentences: list, **kwargs):
+        similarities = self.client.sentence_similarity(model=model, sentence=sentence, other_sentences=other_sentences, **kwargs)
+        return similarities
+
