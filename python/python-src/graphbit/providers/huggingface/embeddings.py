611e42cdd4195eba05bbe2887fe772abe193b6e1
Junaid Hossain <junaid.hossain@infinitibit.com>
2025-11-17 14:47:23 +0600

    removed examples

--- python/python-src/graphbit/providers/huggingface/embeddings.py
+++ python/python-src/graphbit/providers/huggingface/embeddings.py
@@ -4,16 +4,6 @@ class HuggingfaceEmbeddings:
     def __init__(self, token: str, **kwargs):
         self.client = InferenceClient(token=token, **kwargs)
 
-    # def embed(self, model: str, text: str):
-    #     headers = {"Authorization": f"Bearer {self.token}"}
-    #     response = requests.post(
-    #         f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model}",
-    #         headers=headers,
-    #         json={"inputs": text}
-    #     )
-    #     response.raise_for_status()
-    #     return response.json()
-
     def embed(self, model: str, text: str, **kwargs):
         '''Embed the given text using a pre-trained model on Hugging Face Hub.'''''
         embeddings = self.client.feature_extraction(model=model, text=text, **kwargs)
